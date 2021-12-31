bd3a47c6f113a370d8ef1e4f0e615be64cf4661d
RishabhSood <sood.rishabh@hotmail.com>
2021-12-31 16:54:06 +0530

    started lesson 7

---
+++ brownie/smartcontract_lottery/scripts/deploy_lottery.py
@@ -0,0 +1,9 @@
+from scripts.helpful_scripts import get_account
+
+
+def deploy_lottery():
+    account = get_account(id="rinkebyacc2")
+
+
+def main():
+    deploy_lottery()
---
+++ brownie/smartcontract_lottery/scripts/helpful_scripts.py
@@ -0,0 +1,19 @@
+from brownie import accounts, network, config
+
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    elif id:
+        return accounts.load(id)
+    elif(
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or
+        network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts.dev[0]
+    return accounts.add(config["wallets"]["from_key"])
